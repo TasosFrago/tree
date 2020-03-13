3c481575b539b9f46a50be61ea5b79a53e423d54
TasosFrago <tasosfragkopoulos03@gmail.com>
2020-03-13 06:40:39 +0200

    modified:   tree.py

--- tree.py
+++ tree.py
@@ -1,16 +1,6 @@
 import os
 import sys 
 
-def checktype():
-    alls = os.listdir()
-    hidden = []
-    files = []
-    for i in alls:
-        if i[0] == ".":
-            hidden.append(i)
-        else:
-            files.append(i)
-
 def instructions():
     print(f"Usage: {sys.argv[0]} [OPTION]... [PATH]\nPATH is optional.Use it if you want the tree of a specific folder.\nBy default it will use the current path you are in.\n\nOptions:\n  -a  for showing all the files in the directory\n --help  for showing instructions\n")
 
